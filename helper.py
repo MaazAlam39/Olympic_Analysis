13a44a08a583c138c305c777700d97249419cab0
Maaz <itsmaaz39@gmail.com>
2024-07-28 21:21:47 +0530

    Updatted changes in cloud

--- helper.py
+++ helper.py
@@ -51,11 +51,12 @@ def fetch_medal_tally(df, year, country):
 
     return x
 
-def data_over_time(df,col):
-
-    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('index')
-    nations_over_time.rename(columns={'index': 'Edition', 'Year': col}, inplace=True)
+def data_over_time(df, col):
+    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index()
+    nations_over_time = nations_over_time.rename(columns={'index': 'Edition', 'Year': col})
+    nations_over_time = nations_over_time.sort_values('Edition')
     return nations_over_time
+
 
 
 def most_successful(df, sport):
